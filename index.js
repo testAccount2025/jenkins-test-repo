c0b2ddafd8229f626a2844fdacfd3d9a0d403f78
Rafael <rafael.melofernandes2005@outlook.com>
2026-01-16 22:24:09 -0400

    changed server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req,res) => {
-	res.send("hello world from one of pod's container")
+	res.send("hello world from one of pod's container updated")
 });
 
 app.listen(3000, () => console.log("app listening at port 3000"))
